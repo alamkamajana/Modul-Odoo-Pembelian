b4b8903fcefaf0b5a53b435e2f5a528cedc11fa3
alamkamajana <43085227+alamkamajana@users.noreply.github.com>
2022-04-08 08:39:14 +0800

    Change file

--- __manifest__.py
+++ __manifest__.py
@@ -1,36 +1,22 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Pembelian",
 
-    'summary': """ 
+    'summary': """ Modul Pembelian
     """,
 
     'tag': """
-        Long tag of module's purpose
+        Pembelian
     """,
-
-    'author': "",
+    'author': "Alam",
     'website': "",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
-    
     'depends': ['base'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/data_pembelian.xml',
         'views/data_barang.xml',
         'views/data_vendor.xml',
         'views/data_penjualan.xml'
-        
-
-
     ],
-
 }
